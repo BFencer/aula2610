91fe3abcf377804fb25aa0876c686fd468a7ca84
GABRIEL CAMILO DANTAS <33359624866@jpmorgan.sp>
2017-10-26 21:26:24 -0200

    git inicial

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NetOffice.ExcelApi;
+
+namespace aula2610
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Excel excel = new Excel(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+            excel.CriarExcel();
+
+        }
+
+    }
+    class Excel
+    {
+        static string caminho;
+        public Excel(string caminhorecebido)
+        {
+            caminho = caminhorecebido;
+        }
+    
+        public void CriarExcel()
+        {
+            Application ex = new Application();
+            ex.Workbooks.Add();
+            ex.Cells[1, 1].Value = "Ford";
+            ex.ActiveWorkbook.SaveAs(caminho + "\\arquivo.xlsx");
+            ex.Quit();
+        }
+    }
+}
